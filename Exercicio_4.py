5a90c26f5267b0a0b7ff956a429c55f2bf2dec56
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-05 13:23:58 -0300

    Atividades realizadas

---
+++ Exercicio_4.py
@@ -0,0 +1,8 @@
+num=float(input("Digite um número: "))
+if num>=0:
+    raiz=num**(1/2)
+    quadrado=num**2
+    print(f"A raiz do número é {raiz} \nO quadrado do número é {quadrado} ")
+else:
+    print("Operação inválida.")
+    
