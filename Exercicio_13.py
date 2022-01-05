a0c54e278db9cc1cf4f84f23dce0e6864909e46e
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-05 13:29:17 -0300

    Atividades realizadas

---
+++ Exercicio_13.py
@@ -0,0 +1,8 @@
+nota1=float(input("Digite a primeira nota: "))
+nota2=float(input("Digite a segunda nota: "))
+nota3=float(input("Digite a terceira nota: "))
+mediapon=(nota1*1)+(nota2*1)+(nota3*2)/4
+if mediapon>=60:
+    print("Parabéns você foi aprovado!!!")
+else:
+    print("infelizmente você foi reprovado!!!")
