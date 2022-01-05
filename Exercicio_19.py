e6b77bcf5373c81334f9976da2e5d04e00fba254
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-05 13:31:57 -0300

    Atividades realizadas

---
+++ Exercicio_19.py
@@ -0,0 +1,8 @@
+num=int(input("Digite um número: "))
+if num%3==0 and num%5==0:
+    print()
+elif num%5==0:
+    print("O número é divisível por 5")
+elif num%3==0:
+    print("O número é divisível por 3")
+    
