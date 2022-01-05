af38a33318525dcb310d9aefeeb731ccf0a33b6a
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-05 13:22:11 -0300

    Atividades realizadas

---
+++ Exercicio_1.py
@@ -0,0 +1,7 @@
+num1=float(input("Digite um número: "))
+num2=float(input("Digite outro número: "))
+if num1>num2:
+    print(f"O primeiro número ({num1}) é maior.")
+else:
+    print(f"O segundo número ({num2}) é maior.")
+    
